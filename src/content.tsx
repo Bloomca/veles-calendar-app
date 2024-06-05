7427f5419c534afe60ddf7307585da6a1cf82d80
Seva Zaikov <seva.zaikov@gmail.com>
2024-06-04 20:29:57 -0700

    add list view

--- src/content.tsx
+++ src/content.tsx
@@ -1,16 +1,32 @@
+import { createState } from "veles";
+
 import { Calendar } from "./calendar";
+import { ListView } from "./list-view";
 import { createStoreState } from "./store";
 
+import type { State } from "veles";
+import type { ViewSettings } from "./types";
+
 function Content() {
+  const viewSettings = createState<ViewSettings>({
+    type: "calendar",
+  });
   return (
     <div>
-      <ProjectHeader />
-      <Calendar />
+      <ProjectHeader viewSettings={viewSettings} />
+      {viewSettings.useValueSelector(
+        (settings) => settings.type,
+        (type) => (type === "calendar" ? <Calendar /> : <ListView />)
+      )}
     </div>
   );
 }
 
-function ProjectHeader() {
+function ProjectHeader({
+  viewSettings,
+}: {
+  viewSettings: State<ViewSettings>;
+}) {
   const activeProjectState = createStoreState((state) => {
     const projectActiveId = state.activeProject;
 
@@ -24,8 +40,48 @@ function ProjectHeader() {
   });
 
   return (
-    <div>
+    <div class="project-header-container">
       <h1>{activeProjectState.useValue()}</h1>
+      <Settings viewSettings={viewSettings} />
+    </div>
+  );
+}
+
+function Settings({ viewSettings }: { viewSettings: State<ViewSettings> }) {
+  return (
+    <div class="settings-container">
+      <div>
+        <button
+          class={viewSettings.useAttribute((settings) =>
+            settings.type === "list"
+              ? "view-type-button active"
+              : "view-type-button"
+          )}
+          onClick={() =>
+            viewSettings.setValue((currentValue) => ({
+              ...currentValue,
+              type: "list",
+            }))
+          }
+        >
+          List view
+        </button>
+        <button
+          class={viewSettings.useAttribute((settings) =>
+            settings.type === "calendar"
+              ? "view-type-button active"
+              : "view-type-button"
+          )}
+          onClick={() =>
+            viewSettings.setValue((currentValue) => ({
+              ...currentValue,
+              type: "calendar",
+            }))
+          }
+        >
+          Calendar view
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/list-view.tsx
@@ -0,0 +1,87 @@
+import { combineState } from "veles";
+import { createStoreState } from "./store";
+import { selectState } from "./utils";
+
+import type { Section, Task } from "./types";
+import type { State } from "veles";
+
+function ListView() {
+  const sectionsState = createStoreState((state) => {
+    const activeProjectId = state.activeProject;
+    return Object.values(state.sections).filter(
+      (section) => section.projectId === activeProjectId
+    );
+  });
+
+  return (
+    <div class="list-view-container">
+      <NoSection />
+      {sectionsState.useValueIterator<Section>(
+        { key: "id" },
+        ({ elementState }) => (
+          <Section sectionState={elementState} />
+        )
+      )}
+    </div>
+  );
+}
+
+function NoSection() {
+  const tasksState = createStoreState((state) => {
+    const activeProjectId = state.activeProject;
+
+    return Object.values(state.tasks).filter(
+      (task) => task.projectId === activeProjectId && !task.sectionId
+    );
+  });
+
+  return (
+    <div>
+      <TaskList tasksState={tasksState} />
+    </div>
+  );
+}
+
+function Section({ sectionState }: { sectionState: State<Section> }) {
+  const tasksState = createStoreState((state) => state.tasks);
+  const sectionTasksState = selectState(
+    combineState(tasksState, sectionState),
+    ([tasks, section]) =>
+      Object.values(tasks).filter(
+        (task) =>
+          task.sectionId === section.id && task.projectId === section.projectId
+      )
+  );
+
+  return (
+    <div>
+      <h2>{sectionState.useValueSelector((section) => section.name)}</h2>
+      <TaskList tasksState={sectionTasksState} />
+    </div>
+  );
+}
+
+function TaskList({ tasksState }: { tasksState: State<Task[]> }) {
+  return (
+    <div>
+      {tasksState.useValueIterator<Task>({ key: "id" }, ({ elementState }) => (
+        <Task taskState={elementState} />
+      ))}
+    </div>
+  );
+}
+
+function Task({ taskState }: { taskState: State<Task> }) {
+  return (
+    <div class="task-list-task-container">
+      <div
+        class={taskState.useAttribute(
+          (task) => `task-complete task-priority-${task.priority}`
+        )}
+      ></div>
+      {taskState.useValueSelector((task) => task.title)}
+    </div>
+  );
+}
+
+export { ListView };
