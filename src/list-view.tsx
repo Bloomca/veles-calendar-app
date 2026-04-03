dac5f1aac286fdbed1db21aa340bc19525fb206c
Seva Zaikov <seva.zaikov@gmail.com>
2026-04-03 01:12:31 -0700

    render due date in the list view

--- src/list-view.tsx
+++ src/list-view.tsx
@@ -4,6 +4,50 @@ import { InlineGenerator } from "./inline-generator";
 import type { LabelEntity, Section, Task } from "./types";
 import type { State } from "veles";
 
+const shortDateFormatter = new Intl.DateTimeFormat(undefined, {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+});
+
+const shortDateWithYearFormatter = new Intl.DateTimeFormat(undefined, {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+});
+
+function toStartOfDay(date: Date) {
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
+
+function getDueDateVariant(dueDate: Date): "past" | "soon" | "future" {
+  const dueDateStart = toStartOfDay(dueDate);
+  const todayStart = toStartOfDay(new Date());
+  const tomorrowStart = new Date(todayStart);
+  tomorrowStart.setDate(tomorrowStart.getDate() + 1);
+
+  if (dueDateStart.getTime() < todayStart.getTime()) {
+    return "past";
+  }
+
+  if (
+    dueDateStart.getTime() === todayStart.getTime() ||
+    dueDateStart.getTime() === tomorrowStart.getTime()
+  ) {
+    return "soon";
+  }
+
+  return "future";
+}
+
+function formatDueDate(dueDate: Date) {
+  const currentYear = new Date().getFullYear();
+  return dueDate.getFullYear() === currentYear
+    ? shortDateFormatter.format(dueDate)
+    : shortDateWithYearFormatter.format(dueDate);
+}
+
 function ListView() {
   const sectionsState = createStoreState((state) => {
     const activeProjectId = state.activeProject;
@@ -105,15 +149,26 @@ function Task({
         <div class="task-list-task-title">
           {taskState.renderSelected((task) => task.title)}
         </div>
-        <TaskInfo taskLabelsState={taskLabelsState} />
+        <TaskInfo taskState={taskState} taskLabelsState={taskLabelsState} />
       </div>
     </div>
   );
 }
 
-function TaskInfo({ taskLabelsState }: { taskLabelsState: State<LabelEntity[]> }) {
+function TaskInfo({
+  taskState,
+  taskLabelsState,
+}: {
+  taskState: State<Task>;
+  taskLabelsState: State<LabelEntity[]>;
+}) {
   return (
     <div class="task-list-task-info">
+      {taskState.renderSelected((task) => task.dueDate, (dueDate) => (
+        <span class={`task-due-date task-due-date-${getDueDateVariant(dueDate)}`}>
+          {formatDueDate(dueDate)}
+        </span>
+      ))}
       {taskLabelsState.render((labels) =>
         labels.length > 0 ? (
           <div class="task-list-labels">
